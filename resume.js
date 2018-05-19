250c95f4eb1a79471a31a1a22c5fdd5c72768154
bbailey21us <brandonbailey21@gmail.com>
2018-05-19 09:58:42 -0400

    added some JavaScript

--- resume.js
+++ resume.js
@@ -10,7 +10,7 @@ function displayPosition(head,info){
 
 function displaySkill(stuff,cool){
     if(cool===true){
-        console.log("*BAM:"+" "+stuff)
+        console.log("* BAM:"+" "+stuff)
     }else{
         console.log("* "+stuff)
     }
@@ -21,7 +21,20 @@ displayPosition("Career","CEO OMB LLC");
 displayPosition("description","I own a company that makes and distributes energy pills");
 console.log(" ");
 console.log(" ");
-displayPosition("My interests", " ");
+displayPosition("My interests", "");
+displaySkill("college baskteball");
+displaySkill("butler bulldogs");
+displaySkill("eating right");
+displaySkill("learning new things")
+console.log("")
+console.log("")
+displayPosition("My Previous Experience","")
+displaySkill("Ran a company that made and distributed energy pills")
+displaySkill("traded options for myself and family")
+displaySkill("Own 7 rental properties in Anderson,IN")
+console.log("");
+console.log("");
+displayPosition("My skills","");
 for(i=0;i<skills.length;i++){
     displaySkill(skills[i],cool[i]);
 }
